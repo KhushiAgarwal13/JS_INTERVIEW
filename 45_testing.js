a483408f8bd94c230d0b4a3f2a9d435f1295f1b1
Khushi <agarwalkhushi563@gmail.com>
2024-12-24 20:00:56 +0530

    new file added

---
+++ 45_testing.js
@@ -0,0 +1,4 @@
+// hello khushi
+// the difference between for each and map method is
+// map returns the new array , and for each doesn't return any array 
+// and it's just used to traverse the arragiy
